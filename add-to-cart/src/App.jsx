8c42951fba139edd0e0b13adfe12e3f2cc847f08
Hilarry4269 <118233630+Hilarry4269@users.noreply.github.com>
2023-02-07 16:58:12 -0800

    cart

--- add-to-cart/src/App.jsx
+++ add-to-cart/src/App.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 
 function App() {
@@ -55,39 +56,49 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <input
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-        placeholder="Enter item"
-      />
-      <button onClick={addToCart}>Add to Cart</button>
-      <hr />
-      <ul>
-        {cart.map((item) => (
-          <li
-            key={item.id}
-            style={item.checked ? { textDecoration: "line-through" } : {}}
-          >
-            <input
-              type="checkbox"
-              checked={item.checked}
-              onChange={() => updateCheck(item.id)}
-            />
-            {item.name}
-            <span>
-              <button onClick={() => increaseQuantity(item.id)}>&lt;</button>
+ 
+    <div className="container">
             
+          <div className="App">
+         
+              <input
+                type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                placeholder="Enter items"
+              />
+              <br />
+              
+              <button onClick={addToCart}>Add to basket <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAUdJREFUSEu1le0xBEEQhp+LABEgAi4CREAGiAAZnAgQATJwESADIuAiOCKg3qttNTPb83Fnr/9M1e5MP2/3dPeMWLON1uyfFsAh8JIRUvq3OFIDXAI3wCNwlkAegFPgCrjNZaIE2AQ+AK1HThRS/wx8Abvd2uOUAFJ1AbwCcuaZUncA3AGKthmw06nXAan7zACq+3IRVJUFQItUZ5TKyDyA5fYbkELluGS6I0W44d1VCHgD9gbqC/kap2UqFdsDAWZd9FEfWF0PwfjrmzBFaqT7IbwD54AERxHosuaFUv7JFEj6Xdu2rDjSKvIu2va0At6BfROTAqymQ7HLAqKuTgHWA/8BRHPLa7TWVNQiW4j0AE/A8YrVNAVOvPDDb/YGrMLovQ1eBOGEXBbSm7y5aTrp5rsGWKtdAzpXnaatDpv21d7kJielTb9Yvj8ZhmGqxAAAAABJRU5ErkJggg=="/></button>
+              <br />
+              <ul class="list-group">
+                {cart.map((item) => (
+                  <li
+                  class="list-group-item d-flex justify-content-between align-items-center"
+                    key={item.id}
+                    style={item.checked ? { textDecoration: "line-through" } : {}}
+                  >
+                    <input
+                      type="checkbox"
+                      checked={item.checked}
+                      onChange={() => updateCheck(item.id)}
+                    />
+                    {item.name}
+                    <span>
+                    <button onClick={() => decreaseQuantity(item.id)}>&lt;</button>
 
-            {item.quantity}
-            
-              <button onClick={() => decreaseQuantity(item.id)}>&gt;</button>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <hr />
-      Total: {sumCheckedQuantities()}
+                    {item.quantity}
+                    <button onClick={() => increaseQuantity(item.id)}>&gt;</button>
+                      
+                    </span>
+                  </li>
+                ))}
+              </ul>
+              <br/>
+              <h1>
+                  Total: {sumCheckedQuantities()}
+              </h1>
+          </div>
     </div>
   );
 }
